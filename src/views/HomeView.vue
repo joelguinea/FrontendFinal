9e8e97f80f0300936414228c0d333d5da22cd5ab
joel guinea <joelguinea69@gmail.com>
2022-06-09 21:04:45 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<script setup>
+</script>
+<template>
+
+         <img src="/images/ss.jpg" >
+
+        <div class="sd">
+                 <div>
+                   
+                       <p class="card-text"  style="top:58%; position:relative; font-size:95px;">Welcome to My Tshirt Clothing</p>
+                        
+
+              </div>
+           </div>
+        
+
+  
+
+
+        <div class="card" style=" ">
+
+  <div class="card-body bg-gray">
+   
+   
+   
+    
+  </div>
+</div>
+</template>
+
+
+<style scoped>
+
+img{
+    position: absolute;
+    width: 100%;
+    height:90%;
+    top:7%;
+    left:0px;
+}
+
+.ss{
+    position: absolute;
+    top: 17%;
+    left: 13%;
+}
+
+
+.sf{
+    position: absolute;
+    top: 17%;
+    left:67%;
+}
+
+
+</style>
